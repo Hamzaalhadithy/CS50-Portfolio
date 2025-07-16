91caada598a81fa7e0d0e03ed7406a98c6468536
Hamzaalhadithy <hamzaalseade@gmai.com>
2025-07-16 18:55:33 +0300

    Blog style and api

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-import os 
-from flask import Flask, flash ,render_template, redirect, request, session
-from cs50 import SQL
+from flask import Flask, flash ,render_template, redirect, request, session, url_for, jsonify
 from flask_session import Session
+from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = 'alhadeethimo'
 
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+db = SQL("sqlite:///main.db")
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
 
 
 @app.after_request
@@ -23,14 +40,44 @@ def after_request(response):
     return response
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log in for admins"""
+
+    if request.method == "POST":
+        session.clear()
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            flash("Please Enter your info!", "danger")
+            return redirect(url_for('login'))
+        
+        rows = db.execute(
+            "SELECT * FROM admins WHERE username = ?", username
+            )
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+            flash("Your Password is Incorrect", "danger")
+            return redirect(url_for('login'))
+        
+        session['user_id'] = rows[0]["id"]
+        flash("Login successful!", "success")
+        return redirect("/")
+    
+    return render_template("login.html")
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/blog") 
 def blog():
-    return render_template("blog.html")
+    return render_template("blog.html", user_id=session.get("user_id"))
 
+@app.route("/articles")
+def article():
+    articles = db.execute("SELECT * FROM articles")
+    return jsonify(articles), 200
 
 @app.route("/projects")
 def projects():
@@ -39,3 +86,12 @@ def projects():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("Loged Out")
+    return redirect(url_for('index'))
+
+    
--- static/app.js
+++ static/app.js
@@ -44,3 +44,51 @@ if (window.location.pathname == '/contact')
   document.getElementById('contact').classList.add('nav-link-active');
   
 }
+if (window.location.pathname == '/blog')
+{
+  article_container = document.getElementById('article-container');
+  document.addEventListener('DOMContentLoaded', async function(event){
+    let response = await fetch("/articles");
+    let articles = await response.json();
+    let html = ''
+    for(article of articles){
+      let date = new Date(article.timestamp)
+      let formated = date.toLocaleString('en-TR',{
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        }
+      );
+      html +=`
+          <div class="article-card d-flex align-items-center m-2 glassy" id="article">
+            <div class="row gap-2">
+                <div class="d-flex article-image col-lg-3 justify-content-end"><img class="rounded-circle"
+                        style="width: 90%;" src="${article.image_url}" alt="${article.title}" /></div>
+                <div class="d-flex flex-column gap-2 col justify-content-start">
+                    <div class="">
+                        <h2>${article.title}</h2>
+                    </div>
+                    <div class="">
+                        <p>${article.content}</p>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <span style="font-size: 10pt;">Published ${formated}</span>
+                        ${USER_ID ? `
+                            <form action="/blog" method="post">
+                                <input type="hidden" value="${article.id}">
+                                <span class="gap-3">
+                                    <button class="btn btn-primary" name="edit" value="edit">Edit</button>
+                                    <button class="btn btn-danger" name="delete" value="delete">Delete</button>
+                                </span>
+                            </form>
+                        ` : ''}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+      `;
+    }
+    article_container.innerHTML += html;
+  });
+}
